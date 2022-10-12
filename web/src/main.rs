8bb6802eea8569a2c21d830a10b1f655f03ae974
quinn <q.shanahan@gmail.com>
2022-10-12 19:21:26 -0400

    wip

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,36 +1,6 @@
-use yew::prelude::*;
+mod components;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
-    html! {
-        <div class="bg-neutral-900 text-lime-200">
-            <div class="root">
-                <div class="border-r border-neutral-700 p-3" {onclick}>
-                    {"Sidebar"}
-                </div>
-
-                <div>
-                    <div class="border-b border-neutral-700 p-3">
-                        <h1 class="text-3xwl font-bold underline">{"Hello world!"}</h1>
-                    </div>
-
-                    <div class="p-3">
-                        {"body content"}
-                    </div>
-                </div>
-            </div>
-        </div>
-    }
-}
+use crate::components::App;
 
 fn main() {
     yew::Renderer::<App>::new().render();
